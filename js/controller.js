2ded695f30bc2c115f2ca9f84d6479e748333df3
shuangge-jb <562559243@qq.com>
2017-07-16 23:27:17 +0800

    试验了2种依赖注入，2个controller间使用service通信

--- js/controller.js
+++ js/controller.js
@@ -1,21 +1,4 @@
 var myApp=angular.module("myApp",[]);
-
-//声明自定义服务
-myApp.service("myService",function(){
-	this.say=function(str){
-		return "hello "+str+"!";
-	}
-});
-//调用自定义服务
-myApp.controller("myCtrl2",function($scope,myService){
-	$scope.name='';
-	
-	
-	$scope.sayHello=function(){
-		$scope.hello=myService.say($scope.name);
-	}
-});
-
 //调用HTTP服务
 myApp.controller("myCtrl",function($scope,$http){
 $http.get("server.js").then(function(response){
@@ -23,3 +6,48 @@ $http.get("server.js").then(function(response){
 	$scope.data=response.data;
 });
 });
+
+//声明自定义服务
+myApp.service("myService",function(){
+	this.say=function(str){
+		return "hello "+str+"!";
+	}
+});
+//在两个controller间共享数据
+myApp.service("dataShareService",function(){
+	var data='';
+	this.getData=function(){
+		return data;
+	}
+	this.setData=function(obj){
+		data=obj;
+	}
+});
+//调用自定义服务，数组注释法
+myApp.controller("myCtrl2",["$scope","myService","dataShareService",function($scope,myService,dataShareService){
+	$scope.name='';
+	
+	
+	$scope.sayHello=function(){
+
+		$scope.hello=myService.say($scope.name);
+	}
+$scope.setData=function(){
+	dataShareService.setData($scope.name);
+}
+
+}]);
+//显式调用function的$inject
+function logic($scope,dataShareService){
+	$scope.input='666';
+	
+	$scope.update=function(){
+		$scope.input=dataShareService.getData();
+       
+	}
+}
+logic.$inject=["$scope","dataShareService"];
+myApp.controller("myCtrl3",logic);
+
+
+
